439dd852c90cd0eae25788279b453e7526465321
Bruno Santos <brunolsantos152@gmail.com>
2024-05-04 00:17:10 -0300

    feat: new lp

---
+++ app/new-from-scratch/page.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+'use client'
+
+import ScrollUp from '@/components/Common/ScrollUp'
+import Hero from '@/components/Hero'
+import { Inter } from '@next/font/google'
+import { useRef } from 'react'
+import FromScratch from '@/components/NewFromScratch'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  const pricingRef = useRef(null)
+  const contributorsRef = useRef(null)
+  const tallyFormsRef = useRef(null)
+
+  return (
+    <>
+      <ScrollUp />
+      <FromScratch />
+    </>
+  )
+}
