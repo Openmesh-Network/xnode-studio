955d63171984c3514c91c22b3bf6ff2b0a750dcf
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2024-06-05 11:34:27 +0200

    fix: workspace area layout

--- components/Workspace/index.tsx
+++ components/Workspace/index.tsx
@@ -5,10 +5,8 @@ import NodesFlow from '../NodesFlow'
 const Workspace = () => {
   return (
     <>
-      <div className="px-[50px] xl:px-[70px] 2xl:px-[100px]">
-        <div className="mx-auto mb-[50px] h-[1300px] max-w-[300px] rounded-[10px] bg-[#F9F9F9] md:max-w-[600px] lg:max-w-[950px] xl:max-w-[1200px] 2xl:mb-[100px] 2xl:max-w-[1500px] 4xl:max-w-full">
-          <NodesFlow fromScratch={false} />
-        </div>
+      <div className="mx-auto mb-[50px] h-[calc(100svh-4rem)] rounded-[10px] bg-[#F9F9F9] 2xl:mb-[100px]">
+        <NodesFlow fromScratch={false} />
       </div>
     </>
   )
