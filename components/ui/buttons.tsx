22af2b5ce15b2807650737566d4f4ba7db976c5b
Piros <72915404+Hidde-Heijnen@users.noreply.github.com>
2024-06-05 16:26:40 +0200

    fix: primary color working again

--- components/ui/buttons.tsx
+++ components/ui/buttons.tsx
@@ -7,18 +7,18 @@ import { cn } from '@/lib/utils'
 //TODO: These colors are not set up properly, but I would recommend just setting up shadcn for the whole app and then installing the button component again.
 // Currently our (Piros and Jeyprox) job is not to impelement the button or any reusable component, but to fix the existing bugs.
 const buttonVariants = cva(
-  'focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
         default:
-          'duration-400 text-primary-foreground bg-primary shadow-[inset_0_-2px_4px_rgba(0,0,0,0.4)] transition-shadow hover:bg-primary/90',
+          'duration-400 bg-primary text-primary-foreground shadow-[inset_0_-2px_4px_rgba(0,0,0,0.4)] transition-shadow hover:bg-primary/90',
         destructive:
-          'bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm',
+          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border-input hover:bg-accent hover:text-accent-foreground border shadow-sm',
+          'border border-input shadow-sm hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm',
+          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
