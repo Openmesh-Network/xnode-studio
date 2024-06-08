24804e3e9143f1e58f4131e51343aaab51125a94
Piros <72915404+Hidde-Heijnen@users.noreply.github.com>
2024-06-08 11:49:10 +0200

    feat: add header component

--- app/data/documentation/page.tsx
+++ app/data/documentation/page.tsx
@@ -1,3 +1,3 @@
 export default function DataMarketplace() {
-  return <section className="space-y-4 p-12"></section>
+  return <section className="space-y-4 p-12">Test</section>
 }
---
+++ components/ui/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { cn } from '@/lib/utils'
+
+interface HeaderProps
+  extends React.PropsWithChildren<
+    Omit<React.HTMLAttributes<HTMLHeadingElement>, 'level'>
+  > {
+  level?: 1 | 2 | 3 | 4 | 5 | 6
+}
+
+const Header = ({ className, children, level = 1, ...props }: HeaderProps) => {
+  const headerClasses = [
+    'text-5xl font-bold leading-10',
+    'text-2xl font-bold leading-6',
+    'text-xl font-bold',
+    'text-lg font-bold',
+    'text-base font-bold',
+    'text-sm font-bold',
+  ]
+
+  const headerTag = `h${level}`
+
+  // returns a header tag with the appropriate level (h1, h2, etc.), the appropriate class, the rest of the props and the children
+
+  return React.createElement(
+    headerTag,
+    { className: cn(headerClasses[level - 1], className), ...props },
+    children
+  )
+}
+
+export default Header
