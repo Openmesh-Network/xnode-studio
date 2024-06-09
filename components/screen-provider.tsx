02e79cd2f1640c1e4bb5b012eed7efc55a674ad7
Jeyprox <56683209+Jeyprox@users.noreply.github.com>
2024-06-09 16:42:58 +0200

    feat: disable application on mobilr

---
+++ components/screen-provider.tsx
@@ -0,0 +1,53 @@
+'use client'
+
+import { PropsWithChildren, useEffect, useState } from 'react'
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from './ui/dialog'
+
+const MOBILE_BREAKPOINT = 1024
+
+type ScreenProviderProps = PropsWithChildren<{}>
+export default function ScreenProvider({ children }: ScreenProviderProps) {
+  const [showDialog, setShowDialog] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth < MOBILE_BREAKPOINT) {
+      setShowDialog(true)
+    }
+    const handleResize = () => {
+      if (window.innerWidth > MOBILE_BREAKPOINT) {
+        setShowDialog(false)
+      } else {
+        setShowDialog(true)
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  return (
+    <div className="relative">
+      {showDialog ? (
+        <Dialog open>
+          <DialogContent canClose={false}>
+            <DialogHeader>
+              <DialogTitle>Xnode can only be accessed on desktop.</DialogTitle>
+              <DialogDescription>
+                Please use a different device with a larger screen to use the
+                application.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      ) : null}
+      {children}
+    </div>
+  )
+}
