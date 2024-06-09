42ea7798b56557716cc10aa280259b3fb1d8046a
Plopmenz <plopmenz@gmail.com>
2024-06-09 14:44:49 +0200

    xnode subpath rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
       source: '/xue-signer/:call*',
       destination: 'https://remote-signer.plopmenz.com/xue-signer/:call*',
     },
+    {
+      source: '/xnode',
+      destination: '/',
+    },
+    {
+      source: '/xnode/:call*',
+      destination: '/:call*',
+    },
   ],
   basePath: process.env.NEXT_PUBLIC_PREFIX,
   reactStrictMode: false,
