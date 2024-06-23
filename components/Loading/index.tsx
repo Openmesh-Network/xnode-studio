0699226a7aa7fad0820c5be47445e2210c3a58d7
Ansh Mishra <mishra.ansh6@gmail.com>
2024-06-23 16:28:48 +0530

    Loading Component Modified

--- components/Loading/index.tsx
+++ components/Loading/index.tsx
@@ -1,13 +1,29 @@
-
+import { ClipLoader} from "react-spinners"
 
 const Loading = () => {
   return (
-    <>
-      <div className="w-full h-full flex justify-center item-center">
-        <div className="size-24 animate-spin rounded-full border-b-2 border-[#0354EC]"></div>
-      </div>
-    </>
+    <div className="spinner w-full h-full flex justify-center items-center">
+      <ClipLoader
+          color="#0354EC"
+          size={80}
+          speedMultiplier = {1}
+      />
+    </div>
   )
 }
 
 export default Loading;
+
+
+
+// const Loading = () => {
+//   return (
+//     <>
+//       <div className="w-full h-full flex justify-center item-center">
+//         <div className="size-24 animate-spin rounded-full border-b-2 border-[]"></div>
+//       </div>
+//     </>
+//   )
+// }
+
+// export default Loading;
