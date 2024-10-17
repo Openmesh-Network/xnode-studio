4f221a83cf44d368059a9abb9f00b2176f585b67
Plopmenz <plopmenz@gmail.com>
2024-10-17 23:26:00 +0200

    restrict mobile access

--- components/screen-provider.tsx
+++ components/screen-provider.tsx
@@ -2,6 +2,14 @@
 
 import { useEffect, useState, type PropsWithChildren } from 'react'
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog'
+
 const MOBILE_BREAKPOINT = 1024
 
 type ScreenProviderProps = PropsWithChildren<{}>
@@ -9,11 +17,11 @@ export default function ScreenProvider({ children }: ScreenProviderProps) {
   const [showDialog, setShowDialog] = useState(false)
 
   useEffect(() => {
-    if (window.innerWidth < MOBILE_BREAKPOINT) {
+    if (window.visualViewport.width < MOBILE_BREAKPOINT) {
       setShowDialog(true)
     }
     const handleResize = () => {
-      if (window.innerWidth > MOBILE_BREAKPOINT) {
+      if (window.visualViewport.width > MOBILE_BREAKPOINT) {
         setShowDialog(false)
       } else {
         setShowDialog(true)
@@ -24,5 +32,24 @@ export default function ScreenProvider({ children }: ScreenProviderProps) {
     return () => window.removeEventListener('resize', handleResize)
   }, [])
 
-  return <>{children}</>
+  return (
+    <>
+      {showDialog ? (
+        <Dialog open>
+          <DialogContent className="t-50 l-50 absolute" canClose={false}>
+            <DialogHeader>
+              <DialogTitle>
+                Xnode Studio can only be accessed on desktop.
+              </DialogTitle>
+              <DialogDescription>
+                Please use a different device with a larger screen to use the
+                application.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      ) : null}
+      {children}
+    </>
+  )
 }
